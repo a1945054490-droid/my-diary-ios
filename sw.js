24ad4a50a97e1c809faa7992de77bf5030edfb8c
a1945054490-droid <a1945054490@gmail.com>
2026-05-29 04:16:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,41 @@
-const CACHE_NAME = 'diary-v2';
+var CACHE_NAME = 'diary-v2';
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './icon.png'
-      ]);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll([
+                './',
+                './index.html',
+                './icon.png'
+            ]);
+        })
+    );
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
-    })
-  );
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(
+                keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); })
+            );
+        })
+    );
+    self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request).then(function(response) {
+            // 网络成功，更新缓存
+            var clone = response.clone();
+            caches.open(CACHE_NAME).then(function(cache) {
+                cache.put(event.request, clone);
+            });
+            return response;
+        }).catch(function() {
+            // 网络失败（离线），用缓存
+            return caches.match(event.request);
+        })
+    );
 });
